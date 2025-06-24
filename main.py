486dfd61e15cec50c33365a37f6fbef840ff62e5
MeldasHan <72067875+MeldasHan@users.noreply.github.com>
2025-06-24 13:47:19 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from discord import Interaction, ButtonStyle
 from discord.ui import View, Button
+from discord import app_commands
 import requests
 from dotenv import load_dotenv
 from keep_alive import keep_alive
